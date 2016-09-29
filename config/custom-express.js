420f97b87f4f22e992d103a680afaa1748dc37cd
Fred Carneiro <carneiro@outlook.com>
2016-09-29 13:27:37 -0300

    Updated the POST route to use the express-validator in file payments.js Added comments.txt to facilitate the common commands Using HTTP Status Code in POST Route Added status_code.txt to list the used HTTP Status Codes

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var consign = require('consign'); /* autoload */
 var bodyParser = require('body-parser'); /* body parsing middleware */
+var expressValidator = require('express-validator'); /* validation middleware */
 
 module.exports = function(){
   var app = express();
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+  app.use(expressValidator());
 
   consign()
     .include('controllers')
+    .then('persist')
     .into(app);
 
   return app;
--- controllers/payments.js
+++ controllers/payments.js
@@ -5,9 +5,42 @@ module.exports = function (app){
   });
 
   app.post('/pagamentos/pagamento', function(request, response){
+
+    request.assert('payment_form', 'Payment form is required.').notEmpty();
+    request.assert('value', 'Value is required.').notEmpty();
+    request.assert('value', 'Value must be Decimal Number.').isFloat();
+
+    var validation_errors = request.validationErrors();
+
+    if (validation_errors) {
+      console.log('Validation errors:' + validation_errors);
+      response.status(400).send(validation_errors);
+      return;
+    }
+
     var payment = request.body;
-    console.log(payment);
-    response.send('Ok Post.');
+
+    console.log('processing new payment.');
+
+    payment.status = 'CREATED';
+    payment.data = new Date;
+
+    var connection = app.persist.connectionFactory();
+    var paymentDao = new app.persist.PaymentDAO(connection);
+
+    paymentDao.save(payment, function(error, result){
+
+      if (error) {
+        response.status(500).send('Error while inserting:' + error);
+        console.log(error);
+      }else{
+        console.log('payment created');
+        response.location('/payments/payment/' + result.insertId);
+        response.status(201).json(payment);
+      }
+
+    });
+
   });
 
 }
